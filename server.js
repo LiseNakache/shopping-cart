1af535d534ba96d848e506e4d4c722ec03a09ecd
Lise <nakache.lise@gmail.com>
2017-09-10 15:49:49 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+app.listen(8000);
+
+
+
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname + './index.html');
+  });
+
+
