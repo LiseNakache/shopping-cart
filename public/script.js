fa405e94659f2a2ce051f3a46da28ed0ac6603a1
Lise <nakache.lise@gmail.com>
2017-09-13 12:05:41 +0300

    Patner A

--- public/script.js
+++ public/script.js
@@ -1,42 +1,88 @@
 var source = $('#card-template').html();
 var template = Handlebars.compile(source)
 
+//click handlers
 $('.view-cart').on('click', function () {
   $(".shopping-cart").toggle();
 });
 
-// an array with all of our cart items
+$('.cart-list').on('click', '.remove-item', function () {
+  removeItem(this)
+});
+
+$('.clear-cart').on('click', function () {
+  clearCart();
+});
+
+
 var cart = [];
 
-var addItem = function (a, b) {
+var addItem = function (item, price) {
   var cartItem = {
-    item: a,
-    price: b
+    item: item,
+    price: price,
+    quantity: 1
   };
-  cart.push(cartItem);
+
+  var itemExist = false;
+
+  if (cart.length === 0) {
+    cart.push(cartItem);
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+      if (cart[i].item === item) {
+        cart[i].quantity++;
+        cart[i].price = price * (cart[i].quantity);
+        itemExist = true;
+      }
+    }
+    if (itemExist === false) {
+      cartItem.quantity = 1;
+      cart.push(cartItem);
+    }
+  }
 }
 
 
 var updateCart = function (price) {
-  var totalPrice = 0;
-// totalPrice inside the function, or the totalPrice will keep storing the price and adding it again
   $('.cart-list').empty();
   $('.total').empty();
+
   for (i = 0; i < cart.length; i++) {
     var newHTML = template(cart[i]);
     $('.cart-list').append(newHTML);
-    totalPrice += cart[i].price;
-   
   }
-  $('.total').append(totalPrice);
-//append after the for loop or it will append every time, so adding but not additionner
+  _getTotal();
+}
+
+
+var _getTotal = function () {
+  var total = 0;
+  for (i = 0; i < cart.length; i++) {
+    total += cart[i].price;
+  }
+  // $('.total').empty()
+  // $('.total').append(total); --> add a string and not a number
+
+  $('.total').html(total);
 }
 
 var clearCart = function () {
-cart = [];
-updateCart();
+  cart = [];
+  updateCart();
 }
 
+var removeItem = function (btn) {
+  //remove from the array
+  var item = $(btn).closest('.cart-list');
+  var specificItem = $(btn).closest('.item-template').find('p');
+  var itemIndex = specificItem.index();
+  cart.splice(itemIndex, 1);
+
+  // remove from the page
+  specificItem.remove()
+  _getTotal();
+}
 
 $('.add-to-cart').on('click', function () {
   var item = $(this).closest(".item").data().name
@@ -44,13 +90,9 @@ $('.add-to-cart').on('click', function () {
 
   addItem(item, price);
   updateCart(price);
+
+
 });
 
-
-
-$('.clear-cart').on('click', function () {
-  clearCart();
-});
-
-// update the cart as soon as the page loads!
+// update the cart as soon as the page loads!IMPORTANT FOR LOCAL STORAGE
 updateCart();
